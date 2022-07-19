9ba7779e9b0b83b9128a5258fe7b7bf0c0a7e695
Tri Le <trile95.it@gmail.com>
2022-07-19 14:04:20 -0400

    add api route for addition operation

--- Calculator-API/Controllers/CalculatorController.cs
+++ Calculator-API/Controllers/CalculatorController.cs
@@ -2,26 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Calculator_Logic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Calculator_API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class CalculatorController : ControllerBase
     {
         private readonly ILogger<CalculatorController> _logger;
+        private readonly Calculator calc;
 
         public CalculatorController(ILogger<CalculatorController> logger)
         {
             _logger = logger;
+            calc = new Calculator();
         }
 
+        // Create an API route for addition operation via HttpGet request
+        // with left and right query params specified on the HttpRequest URL
         [HttpGet]
-        public ActionResult Get()
+        [Route("add/{left}/{right}")]
+        public double Add(double left, double right)
         {
-            return Ok();
+            return calc.Add(left, right);
         }
     }
 }

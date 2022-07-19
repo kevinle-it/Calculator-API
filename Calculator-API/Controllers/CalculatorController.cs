fb0e167d44f84ab2569442d6f8bde57e97b12f9b
Tri Le <trile95.it@gmail.com>
2022-07-19 14:11:16 -0400

    add api route for division operation

--- Calculator-API/Controllers/CalculatorController.cs
+++ Calculator-API/Controllers/CalculatorController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Calculator_Logic;
 using Microsoft.AspNetCore.Mvc;
@@ -46,6 +47,25 @@ namespace Calculator_API.Controllers
         public double Multiply(double left, double right)
         {
             return calc.Multiply(left, right);
+        }
+
+        // Create an API route for division operation via HttpGet request
+        // with left and right query params specified on the HttpRequest URL.
+        // This will return response with "400 Bad Request" HttpStatusCode
+        // and "Attempted to divide by zero." message if
+        // the HttpRequest URL contains "right" parameter as number 0.
+        [HttpGet]
+        [Route("divide/{left}/{right}")]
+        public ObjectResult Divide(double left, double right)
+        {
+            try
+            {
+                return Ok(calc.Divide(left, right));
+            }
+            catch (Exception ex)
+            {
+                return Problem(detail: ex.Message, statusCode: (int)HttpStatusCode.BadRequest);
+            }
         }
     }
 }
